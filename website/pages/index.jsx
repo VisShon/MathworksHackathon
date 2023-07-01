1e9faa15c1046717104e24e6fa7dd92ba17a2470
Vishnu Shon <88622653+VisShon@users.noreply.github.com>
2023-07-01 22:59:05 +0530

    updates

--- website/pages/index.jsx
+++ website/pages/index.jsx
@@ -1,113 +1,35 @@
 import Head from 'next/head'
 import Timings from '@/components/Timings'
 import ManagerInfoPanel from '@/components/ManagerInfoPanel'
-import { useState } from 'react'
+
+import GetInterviewer from '@/apollo/query/getInterviewer.graphql'
+import { useState, useEffect } from 'react'
+import { useQuery } from '@apollo/client'
+import nProgress from 'nprogress'
 
 export default function Home() {
-	const [managerData, setManagerData] = useState({
-		id:'1',
-		name:'R. Raghavendra',
-		mail: 'vishnu20414@iiitd.ac.in',
-		role:'EDG Manager',
-		image:'/ManagerProfile.png',
-		skills:['Management','QE','Development','Business'],
-		timings: [
-			{
-				id:'vishnu',
-				name:'Vishnu Shon',
-				branch:'Computer Science and Design',
-				role:'Developer',
-				college:'IIITD',
-				start:'9:30',
-				end:'10:30',
-			},
-			{
-				id:'vishnu',
-				name:'Vishnu Shon',
-				branch:'Computer Science and Design',
-				role:'Manager',
-				college:'IIITD',
-				start:'9:30',
-				end:'10:30',
-			},
-			{
-				id:'vishnu',
-				name:'Vishnu Shon',
-				branch:'Computer Science and Design',
-				role:'Manager',
-				college:'IIITD',
-				start:'9:30',
-				end:'10:30',
-			},
-			{
-				id:'vishnu',
-				name:'Vishnu Shon',
-				branch:'Computer Science and Design',
-				role:'Manager',
-				college:'IIITD',
-				start:'9:30',
-				end:'10:30',
-			},
-			{
-				id:'vishnu',
-				name:'Vishnu Shon',
-				branch:'Computer Science and Design',
-				role:'Manager',
-				college:'IIITD',
-				start:'9:30',
-				end:'10:30',
-			},
-			{
-				id:'vishnu',
-				name:'Vishnu Shon',
-				branch:'Computer Science and Design',
-				role:'Manager',
-				college:'IIITD',
-				start:'9:30',
-				end:'10:30',
-			},
-
-			{
-				id:'vishnu',
-				name:'Vishnu Shon',
-				branch:'Computer Science and Design',
-				role:'Manager',
-				college:'IIITD',
-				start:'9:30',
-				end:'10:30',
-			},
-			{
-				id:'vishnu',
-				name:'Vishnu Shon',
-				branch:'Computer Science and Design',
-				role:'Manager',
-				college:'IIITD',
-				start:'9:30',
-				end:'10:30',
-			},
-		]
+	const [managerData, setManagerData] = useState()
+	const { loading, error, data } = useQuery(GetInterviewer,{
+		variables:{
+			where:{
+				interviewerId:1
+			}
+		}
 	})
 
-	// const { loading, error, data } = useQuery(GetCandidate,{
-	// 	variables:{
-	// 		where:{
-	// 			id:candidateId
-	// 		}
-	// 	}
-	// })
-	
-	// useEffect(() => {
-	// 	if(loading){
-	// 		nProgress.start()
-	// 	}
-	// 	if(!loading){
-	// 		nProgress.done(false)
-	// 		setEventData(data?.events[0])
-	// 	}
-	// 	if(error){
-	// 		nProgress.done(false)
-	// 	}
-	// },[])
+	useEffect(() => {
+		if(loading){
+			nProgress.start()
+		}
+		if(!loading){
+			nProgress.done(false)
+			setManagerData(data)
+		}
+		if(error){
+			nProgress.done(false)
+		}
+	},[])
+
 	return (
 		<main>
 			<Head>
@@ -126,10 +48,10 @@ export default function Home() {
 			<div className='flex w-full p-10'>
 				<div className='flex flex-col w-[30%] px-10 relative'>
 					<ManagerInfoPanel
-						image={managerData.image} 
-						name={managerData.name} 
-						role={managerData.role}
-						skills={managerData.skills}
+						image={managerData?.image} 
+						name={managerData?.name} 
+						role={managerData?.role}
+						skills={managerData?.skills}
 					/>
 				</div>
 
