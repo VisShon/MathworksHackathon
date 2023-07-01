7234b6cec56fd38f7d3b3982288f24e9f527558b
Vishnu Shon <88622653+VisShon@users.noreply.github.com>
2023-07-01 11:27:14 +0530

    updated head

--- website/pages/index.jsx
+++ website/pages/index.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
+import Head from 'next/head'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
